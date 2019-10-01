853232a3c6e1ffa261db16b6b4484951bfba9e1a
Tanner Patterson <tpatterson5492@gmail.com>
2019-10-01 03:54:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -56,3 +56,5 @@ def using_delete(array, string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
     no_offense_steven = array.delete("Steven")
 end
+
+def using_delete_at(array, integer)
